4f90dc0e1416a64b6d9991c295dc24b900c5cc1d
Lixmna <kang7145471@naver.com>
2021-01-17 18:16:16 +0900

    Counter Props와 state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Myname from './MyName';
+import Counter from './Counter';
+// import Myname from './MyName';
 
 class App extends React.Component {
   render() {
-
     return (
-      <Myname/>
+      <Counter/>
     );
   }
 }
--- src/MyName.js
+++ src/MyName.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-class Myname extends React.Component {
-    static defaultProps = {
-        name: '기본이름'
-    }
-    render() {
-        return (
-            <div>
-                안녕하세요 ! 제 이름은 <b>{this.props.name}</b> 입니다.
-            </div>
-        )
-    }
+const Myname = ({ name }) => {
+    return (
+        <div>
+            안녕하세요! 제 이름은 {name} 입니다.
+        </div>
+    )
 }
+
+Myname.defaultProps = {
+    name: '기본이름'
+};
 
 export default Myname;
